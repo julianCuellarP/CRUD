b54be858e3fa02628649990dc7b0713d24b0f4ae
julianCuellarP <juliancuellarp@gmail.com>
2022-08-11 10:02:29 -0500

    inicio

--- src/main/java/co/edu/unisabana/CRUD/POST/TeamPost.java
+++ src/main/java/co/edu/unisabana/CRUD/POST/TeamPost.java
@@ -1,4 +1,13 @@
 package co.edu.unisabana.CRUD.POST;
 
+import co.edu.unisabana.CRUD.DTO.TeamDTO;
+
+import java.util.List;
+
 public class TeamPost {
+
+    public List<TeamDTO> create(){
+
+
+    }
 }
--- src/main/java/co/edu/unisabana/CRUD/controller/TeamController.java
+++ src/main/java/co/edu/unisabana/CRUD/controller/TeamController.java
@@ -2,15 +2,19 @@ package co.edu.unisabana.CRUD.controller;
 
 import co.edu.unisabana.CRUD.DELETE.TeamDelete;
 import co.edu.unisabana.CRUD.DTO.TeamDTO;
+import co.edu.unisabana.CRUD.POST.TeamPost;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TeamController {
+
+    ArrayList teams;
 
     @GetMapping("/search")
     public List<TeamDTO> busqueda(@RequestParam String q) {
@@ -25,6 +29,7 @@ public class TeamController {
 
     @GetMapping("/post")
     public void post() {
+        TeamPost subir = new TeamPost();
 
     }
 
